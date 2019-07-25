6d2bfb7a136e07bb21dde834c940a4b0e785f4f8
saurabhk06 <sk702199@gmail.com>
2019-07-25 20:20:46 +0530

    Changes in the ProductResource.php for Product details, addition of more fields

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,12 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' =>$this->discount
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'totalPrice' => round((1 - ($this->discount/100))* $this->price, 2),
+            'discount' =>$this->discount,
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
